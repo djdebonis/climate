bc564d6d1ef37b5abd5fbe24ebcd75e89363a035
djdebonis <davidjdebonis@gmail.com>
2023-01-29 12:08:29 -0700

    fix main

--- temp_output.py
+++ temp_output.py
@@ -10,7 +10,7 @@ import Adafruit_DHT
 #global variables
 
 # time (in seconds) between temperature grabs
-interval = 600
+interval = 10
 
 # column names for output file
 columns = ["date", "time", "temperature", "humidity"]
@@ -33,37 +33,35 @@ def read_temp_and_humidity(sensor = 11, pin = 4):
     return None, None
 
 def main():
-        while True:
+    while True:
 
-            temperature, humidity = read_temp_and_humidity(11,4)
+        temperature, humidity = read_temp_and_humidity(11,4)
 
-            current_dateTime = datetime.now()
-            current_date = current_dateTime.strftime("%Y-%m-%d")
-            current_time = current_dateTime.strftime("%H:%M:%S")
-            new_row = [current_date, current_time, temperature, humidity]
+        current_dateTime = datetime.now()
+        current_date = current_dateTime.strftime("%Y-%m-%d")
+        current_time = current_dateTime.strftime("%H:%M:%S")
+        new_row = [current_date, current_time, temperature, humidity]
 
-            # if there is already a .csv, don't rewrite columns
-            if os.path.exists("temperatures_basement.csv"):
-                # a for append
-                csvfile =  open("temperatures_basement.csv", "a", newline="")
-                csvwriter = csv.writer(csvfile)
-                csvwriter.writerow(new_row)
-                csvfile.close()
+        # if there is already a .csv, don't rewrite columns
+        if os.path.exists("temperatures_basement.csv"):
+            # a for append
+            csvfile =  open("temperatures_basement.csv", "a", newline="")                csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(new_row)
+            csvfile.close()
 
-            # elif the path doesn't exist, make sure to write column names
-            else:
-                csvfile = open("temperatures_basement.csv", "w", newline="")
-                csvwriter = csv.writer(csvfile)
+        # elif the path doesn't exist, make sure to write column names
+        else:
+            csvfile = open("temperatures_basement.csv", "w", newline="")
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(columns)
+            csvwriter.writerow(new_row)
 
-                csvwriter.writerow(columns)
-                csvwriter.writerow(new_row)
+            csvfile.close()
 
-                csvfile.close()
+        print(humidity)
+        print(temperature)
 
-            print(humidity)
-            print(temperature)
-
-            time.sleep(interval)
+        time.sleep(interval)
 
 if __name__ == "__main__":
     main()
