7fd41b4eff7eae2a0605686a97c2962ef6723b38
djdebonis <davidjdebonis@gmail.com>
2023-01-29 11:53:53 -0700

    break into functions

--- temp_output.py
+++ temp_output.py
@@ -5,40 +5,68 @@ import time
 import os
 
 from datetime import datetime
-
 import Adafruit_DHT
 
-interval = 5
+#global variables
 
+# time (in seconds) between temperature grabs
+interval = 600
+
+# column names for output file
 columns = ["date", "time", "temperature", "humidity"]
 
-while True:
 
-    humidity, temperature = Adafruit_DHT.read_retry(11,4)
+def getData(sensor = 11, pin = 4):
+    humidity, temperature = Adafruit_DHT.read_retry(sensor,pin)
 
-    current_dateTime = datetime.now()
-    current_date = current_dateTime.strftime("%Y-%m-%d")
-    current_time = current_dateTime.strftime("%H:%M:%S")
-    new_row = [current_date, current_time, temperature, humidity]
-
-    if os.path.exists("temperatures_basement.csv"):
-        csvfile =  open("temperatures_basement.csv", "a", newline="")
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(new_row)
-        csvfile.close()
-
-    else:
-        csvfile = open("temperatures_basement.csv", "w", newline="")
-        csvwriter = csv.writer(csvfile)
-
-        csvwriter.writerow(columns)
-        csvwriter.writerow(new_row)
-
-        csvfile.close()
+    return(humidity, temperature)
 
 
-    print(humidity)
-    print(temperature)
-    
 
-    time.sleep(interval)
+def read_temp_and_humidity(sensor = 11, pin = 4):
+    temp_c, humidity = getData(sensor,pin)
+
+     if temp_c:
+         temp_f = temp_c * (9 / 5) + 32
+         return round(temp_f, 2), round(humidity, 2)
+
+     return None, None
+
+
+
+
+if "__main__" == __name__:
+
+    while True:
+
+        temperature, humidity = read_temp_and_humidity(11,4)
+
+        current_dateTime = datetime.now()
+        current_date = current_dateTime.strftime("%Y-%m-%d")
+        current_time = current_dateTime.strftime("%H:%M:%S")
+        new_row = [current_date, current_time, temperature, humidity]
+
+        # if there is already a .csv, don't rewrite columns
+        if os.path.exists("temperatures_basement.csv"):
+            # a for append
+            csvfile =  open("temperatures_basement.csv", "a", newline="")
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(new_row)
+            csvfile.close()
+
+        # elif the path doesn't exist, make sure to write column names
+        else:
+            csvfile = open("temperatures_basement.csv", "w", newline="")
+            csvwriter = csv.writer(csvfile)
+
+            csvwriter.writerow(columns)
+            csvwriter.writerow(new_row)
+
+            csvfile.close()
+
+
+        print(humidity)
+        print(temperature)
+
+
+        time.sleep(interval)
